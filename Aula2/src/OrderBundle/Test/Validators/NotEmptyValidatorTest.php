82df674c1409bffd0502e87ae3d6dd9ecf0af2c9
Patrick Siqueira <psique.estuda@gmail.com>
2023-03-19 15:40:01 -0300

    Instalação do composer, inserção do projeto, e criação de dois cenários de teste

--- Aula2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
+++ Aula2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -1,26 +1,25 @@
 <?php
 
 namespace OrderBundle\Validators\Test;
+
 use OrderBundle\Validators\NotEmptyValidator;
 use PHPUnit\Framework\TestCase;
 
 class NotEmptyValidatorTest extends TestCase {
-    public function testShouldNotBeValidWhenValueIsEmpty(){
+    public function testIsValid(){
 
-        $emptyValue = "";
-        $notEmptyValidator = new NotEmptyValidator($emptyValue);
+        $dataProvider = [
+            "" => false,
+            "foo" => false
+        ];
 
-        $isValid = $notEmptyValidator->isValid();
+        foreach ($dataProvider as $value => $expectedResult) {
 
-        $this->assertFalse($isValid);
-    }
+            $notEmptyValidator = new NotEmptyValidator($value);
 
-    public function testShouldBeValidWhenValueIsBotEmpty(){
+            $isValid = $notEmptyValidator->isValid();
 
-        $notEmptyValue = "foo";
-        $notEmptyValidator = new NotEmptyValidator($notEmptyValue);
-
-        $isValid = $notEmptyValidator->isValid();
-        $this->assertTrue($isValid);
+            $this->assertEquals($expectedResult, $isValid);
+        }
     }
 }
