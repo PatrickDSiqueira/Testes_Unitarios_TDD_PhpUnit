52d2e49a6876607c8babadd0deeea8ff4f817686
Patrick Siqueira <psique.estuda@gmail.com>
2023-03-19 11:52:34 -0300

    Criação da Classe de testa da calculadora e visão das etapas de teste

---
+++ Aula1/DiscountCalculatorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+Class DiscountCalculatorTest {
+
+    public function ShouldApply_WhenValueIsAboveTheMinimumTest(){
+
+        // Três fases do teste
+            // * Preparação
+            // * Execução
+            // * Asserção
+
+
+        // 1 - Preparação
+        $discountCalculator = new DiscountCalculator();
+
+        // 2 - Execução
+        $totalValue = 130;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        // 3 - Asserção
+        $expectedValue = 110;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+    }
+
+    public function assertEquals($expectedValue, $actualValue){
+
+        if ($expectedValue !== $actualValue){
+            $message = 'Expected:' . $expectedValue . 'but got: ' . $actualValue;
+            throw new \Exception($message);
+        }
+
+        echo "Test passed! \n";
+    }
+}
