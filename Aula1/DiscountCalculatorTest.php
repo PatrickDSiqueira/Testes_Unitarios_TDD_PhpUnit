f53dbe74a326a32c4ab0e44a38033173dabea2fe
Patrick Siqueira <psique.estuda@gmail.com>
2023-03-19 12:29:55 -0300

    Criação de um novo cenário de teste

--- Aula1/DiscountCalculatorTest.php
+++ Aula1/DiscountCalculatorTest.php
@@ -22,6 +22,17 @@ Class DiscountCalculatorTest {
         $this->assertEquals($expectedValue, $totalWithDiscount);
     }
 
+    public function ShouldNotApply_WhenValueIsBellowTheMinimumTest(){
+
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 90;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 90;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+    }
+
     public function assertEquals($expectedValue, $actualValue){
 
         if ($expectedValue !== $actualValue){
